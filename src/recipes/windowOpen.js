df5201627a152ce98876ca21c31a1fc4be5071f8
Prabs Chawla <pchawla@microsoft.com>
2017-10-26 10:32:05 -0700

    Add try catch to window.open js file

--- src/recipes/windowOpen.js
+++ src/recipes/windowOpen.js
@@ -8,21 +8,27 @@
 void function() {
     window.CSSUsage.StyleWalker.recipesToRun.push( function windowOpen(/*HTML DOM Element*/ element, results) {
 
+    results["use"] = results["use"] || { count: 0};
+    
         if(element.nodeName == "SCRIPT") {
             var api = "window.open(";
-            results["use"] = results["use"] || { count: 0};
             
             // if inline script. ensure that it's not our recipe script and look for string of interest
             if (element.text !== undefined && element.text.indexOf(api) != -1) {
                 results["use"].count += (element.innerText.match(/window.open\(/g) || []).length;
             }
             else if (element.src !== undefined && element.src != "" && element.src.indexOf("Recipe.min.js") == -1) {
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", element.src, false);
-                xhr.send();
+                try {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", element.src, false);
+                    xhr.send();
 
-                if (xhr.status === 200 && xhr.responseText.indexOf(api) != -1) {
-                    results["use"].count += (xhr.responseText.match(/window.open\(/g) || []).length;
+                    if (xhr.status === 200 && xhr.responseText.indexOf(api) != -1) {
+                        results["use"].count += (xhr.responseText.match(/window.open\(/g) || []).length;
+                    }
+                }
+                catch(err) {
+                    // do nothing
                 }
             }
         }
